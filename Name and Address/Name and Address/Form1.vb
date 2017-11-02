59e50c3922027fd104583f9087c7c4401ba17c9b
Bailey <33033378+bp131432@users.noreply.github.com>
2017-11-02 10:33:00 -0500

    Created Name and Address

---
+++ Name and Address/Name and Address/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnShowInfo_Click(sender As Object, e As EventArgs) Handles btnShowInfo.Click
+        lblName.Visible = True
+        lblStreet.Visible = True
+        lblCityStateZip.Visible = True
+    End Sub
+End Class
